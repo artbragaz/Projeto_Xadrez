194e2e609bc96edd6ada79ce8093a913d63de43a
Arthur <artbragaz@gmail.com>
2025-03-12 15:47:47 -0300

    Começando a implementar a classe Board e a classe Piece

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import boardgame.Board;
 import boardgame.Position;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    Position pos = new Position(3, 5);
-    System.out.println(pos);
+   Board board = new Board(8,8);
     }
 }
