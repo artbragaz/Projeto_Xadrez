9376b451e500fded5626ed0ab3f186257812177d
Arthur <artbragaz@gmail.com>
2025-03-12 15:38:06 -0300

    First class: Position

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import boardgame.Position;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    System.out.print("Hello");
+    Position pos = new Position(3, 5);
+    System.out.println(pos);
     }
 }
