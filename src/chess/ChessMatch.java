475dcda3268ce804f4da1db728c1e7082cea510a
Arthur <artbragaz@gmail.com>
2025-03-16 23:02:19 -0300

    Adicionando a Rainha.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -164,6 +164,7 @@ public class ChessMatch {
 
     private void initialSetup(){
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
@@ -180,6 +181,7 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
